4b7c37de6881011fe1d6bb00dd3164ec5c98cf4f
Marin Bareta <mbareta@extensionengine.com>
2018-04-26 21:21:53 +0200

    Strip start number tags

--- app/models/start_number.rb
+++ app/models/start_number.rb
@@ -1,4 +1,11 @@
 class StartNumber < ApplicationRecord
   has_many :race_results
   belongs_to :race, optional: true
+  before_save :strip_tag_id
+
+  private
+
+  def strip_tag_id
+    self.tag_id = tag_id.strip if self.tag_id.present?
+  end
 end

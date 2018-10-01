1ae7b32f61b1774eaaaef99f4e56537a533eaf31
Marin Bareta <marin.bareta@hotmail.com>
2018-10-01 19:20:11 +0200

    Add leagues public page and style it up a bit

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,5 +1,5 @@
 class LeaguesController < ApplicationController
-  before_action :only_admin
+  before_action :only_admin, except: [:index]
   before_action :set_league, only: [:show, :edit, :update, :destroy]
 
   # GET /leagues
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :leagues
+  resources :leagues, path: :natjecanja
   resources :pools
   devise_for :users
   root to: 'dashboard#info'

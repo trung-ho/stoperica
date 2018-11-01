0a0a0198c4cbe8e6a123c6a76d1ca41a5ae51b10
Marin Bareta <marin.bareta@hotmail.com>
2018-11-01 18:12:44 +0100

    Change finals winning order

--- app/models/race_result.rb
+++ app/models/race_result.rb
@@ -189,7 +189,7 @@ class RaceResult < ApplicationRecord
       .race_results
       .select{ |rr| rr.category == category }
       .select { |rr| rr.climbs.dig('final', 'position') }
-    res.sort_by { |rr| [rr.climbs['final']['position'], rr.climbs['final']['time']] }
+    res.sort_by { |rr| [rr.climbs['final']['position'], rr.climbs['q']['position'], rr.climbs['final']['time']] }
       .each_with_index do |rr, index|
       rr.update_column(:position, index + 1)
     end

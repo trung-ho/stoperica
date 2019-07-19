3f992f4b7c99cb70a76b9505d9a973d3fe9e1fa7
Zaid Akram <hello@zaidakram.com>
2019-07-19 19:54:31 +0500

    Added ability to skip auth for devices while updating races

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -132,7 +132,7 @@ class RacesController < ApplicationController
       :name, :date, :laps, :easy_laps, :description_url, :send_email,
       :registration_threshold, :categories, :email_body, :lock_race_results,
       :uci_display, :race_type, :pool_id, :league_id, :control_points_raw,
-      :picture_url, :location_url, :hidden, :started_at, :millis_display
+      :picture_url, :location_url, :hidden, :started_at, :millis_display, :skip_auth
     )
   end
 

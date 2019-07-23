1d9ce015a743970b002cb1ebfffcbca373ae52f3
Zaid Akram <hello@zaidakram.com>
2019-07-23 17:34:11 +0500

    Changed registration fields order

--- app/assets/javascripts/racers.js
+++ app/assets/javascripts/racers.js
@@ -1,2 +1,11 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+document.addEventListener('turbolinks:load', function () {
+  document.getElementById('is_biker_select').addEventListener('change', function () {
+    var uclIdField = document.getElementById('uci_id_field');
+    if (this.value === "1") {
+      uclIdField.style.display = 'block';
+    }
+    else {
+      uclIdField.style.display = 'none';
+    }
+  })
+});

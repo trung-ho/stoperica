4a112e3ca2d53ab27b9cabfb017d24fb144ec90f
Azhar Sajjad <azhar.sajjad@amrood.de>
2019-09-13 17:43:08 +0500

    New League named stage compatitors only is Added!

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -11,6 +11,7 @@ class LeaguesController < ApplicationController
   # GET /leagues/1
   # GET /leagues/1.json
   def show
+    @categories = @league.xczld? ? Category.categories.except(:muskarci) : Category.generics
     @ranks, @base_time = @league.general_rank
     @races_count = @league.races.count
     @racers_count = @ranks.size
